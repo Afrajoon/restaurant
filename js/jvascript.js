25d768576e9e3173f05228cda2d67cb76959cdda
codein.83 <afra.138381@gmail.com>
2025-01-03 12:55:59 +0330

    add picture and creat menu foods

---
+++ js/jvascript.js
@@ -0,0 +1,9 @@
+function hide_sidebar(){
+    const sidebar= document.querySelector('.sidebar');
+    sidebar.style.display='none';
+}
+
+function show_sidebar(){
+    const sidebar= document.querySelector('.sidebar');
+    sidebar.style.display='flex';
+}
