bda5dd3a00dc82113d7badebb76b5e22620fd3d9
codein.83 <afra.138381@gmail.com>
2025-01-09 20:06:16 +0330

    comi

--- js/jvascript.js
+++ js/jvascript.js
@@ -7,3 +7,13 @@ function show_sidebar(){
     const sidebar= document.querySelector('.sidebar');
     sidebar.style.display='flex';
 }
+
+function hide_box(){
+    const sidebar= document.querySelector('.box-sign');
+    sidebar.style.display='none';
+}
+
+function show_box(){
+    const sidebar= document.querySelector('.box-sign2');
+    sidebar.style.display='flex';
+}
