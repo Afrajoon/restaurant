d43de9afdf1ded738ea00685b89a8778147738a9
codein.83 <afra.138381@gmail.com>
2025-01-17 21:08:00 +0330

    responsiv

--- js/jvascript.js
+++ js/jvascript.js
@@ -1,19 +1,65 @@
-// function hide_sidebar(){
-//     const sidebar= document.querySelector('.sidebar');
-//     sidebar.style.display='none';
-// }
+function show_box_signin(){
+    const signin= document.querySelector('.box-signin');
+    signin.style.display='block';
+    const login= document.querySelector('.box-signup');
+    login.style.display='none';
+}
 
-// function show_sidebar(){
-//     const sidebar= document.querySelector('.sidebar');
-//     sidebar.style.display='flex';
-// }
+function show_box_signup(){
+    const login= document.querySelector('.box-signup');
+    login.style.display='block';
+    const signin= document.querySelector('.box-signin');
+    signin.style.display='none';
+}
 
-// function hide_box(){
-//     const login= document.querySelector('.box-signin');
-//     login.style.display='none';
-// }
 
-// function show_box(){
-//     const login= document.querySelector('.box-signup');
-//     login.style.display='flex';
-// }
+let startX = 0;
+let active = 0;
+let isDown = false;
+const speedDrag = -0.1;
+const getZindex = (array, index) =>
+  array.map((_, i) =>
+    index === i ? array.length : array.length - Math.abs(index - i)
+  );
+const carousel = document.querySelector(".carousel");
+const $items = document.querySelectorAll(".carousel-item");
+let progress = (10 * $items.length) / 2;
+const displayItems = (item, index, active) => {
+  const zIndex = getZindex([...$items], active)[index];
+  item.style.setProperty("--zIndex", zIndex);
+  item.style.setProperty("--active", (index - active) / $items.length);
+  item.style.setProperty("--items", $items.length);
+};
+const animate = () => {
+  progress = Math.max(0, Math.min(progress, $items.length * 10));
+  active = Math.floor((progress / ($items.length * 10)) * ($items.length - 1));
+  $items.forEach((item, index) => displayItems(item, index, active));
+};
+animate();
+$items.forEach((item, i) => {
+  item.addEventListener("click", () => {
+    progress = (i / $items.length) * $items.length * 10 + 10;
+    animate();
+  });
+});
+const handleMouseMove = (e) => {
+  if (!isDown) return;
+  const x = e.clientX || (e.touches && e.touches[0].clientX) || 0;
+  const mouseProgress = (x - startX) * speedDrag;
+  progress = progress + mouseProgress;
+  startX = x;
+  animate();
+};
+const handleMouseDown = (e) => {
+  isDown = true;
+  startX = e.clientX || (e.touches && e.touches[0].clientX) || 0;
+};
+const handleMouseUp = () => {
+  isDown = false;
+};
+carousel.addEventListener("mousedown", handleMouseDown);
+carousel.addEventListener("mousemove", handleMouseMove);
+carousel.addEventListener("mouseup", handleMouseUp);
+carousel.addEventListener("touchstart", handleMouseDown);
+carousel.addEventListener("touchmove", handleMouseMove);
+carousel.addEventListener("touchend", handleMouseUp);
